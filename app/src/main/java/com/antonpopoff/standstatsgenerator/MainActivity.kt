58853961e5c438d957554b8ae4ef85d5da6831f2
Anton Popoff <panton295@gmail.com>
2018-07-22 18:11:29 +0300

    Add icons on floating action button

--- app/src/main/java/com/antonpopoff/standstatsgenerator/MainActivity.kt
+++ app/src/main/java/com/antonpopoff/standstatsgenerator/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.antonpopoff.standstatsgenerator
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomSheetBehavior
+import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.antonpopoff.standstatsgenerator.utils.extensions.expanded
 import com.antonpopoff.standstatsgenerator.utils.extensions.toggle
 import com.antonpopoff.standstatsgenerator.utils.listeners.SimpleBottomSheetCallback
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,6 +20,11 @@ class MainActivity : AppCompatActivity() {
         initBottomSheetBehavior()
     }
 
+    private fun setBottomSheetControlButtonIcon() {
+        val imageRes = if (bottomSheetBehavior.expanded()) R.drawable.ic_arrow_down else R.drawable.ic_arrow_up
+        bottomSheetControlButton.setImageResource(imageRes)
+    }
+
     private fun initBottomSheetBehavior() {
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetContainer).apply {
             setBottomSheetCallback(BottomSheetOffsetCallback())
@@ -26,6 +32,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private inner class BottomSheetOffsetCallback : SimpleBottomSheetCallback() {
+
+        override fun onStateChanged(bottomSheet: View, newState: Int) {
+            if (newState == BottomSheetBehavior.STATE_EXPANDED || newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                setBottomSheetControlButtonIcon()
+            }
+        }
 
         override fun onSlide(bottomSheet: View, slideOffset: Float) {
             bottomSheetControlButton.translationY = -bottomSheet.height * slideOffset
