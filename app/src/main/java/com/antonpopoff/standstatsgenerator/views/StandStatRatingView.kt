d2c805923c0da1b0d1e2524d9d91a94af6beec5f
Anton Popoff <panton295@gmail.com>
2018-07-23 20:38:58 +0300

    Start working on stat rating chooser view

--- app/src/main/java/com/antonpopoff/standstatsgenerator/views/StandStatRatingView.kt
+++ app/src/main/java/com/antonpopoff/standstatsgenerator/views/StandStatRatingView.kt
@@ -1,4 +1,19 @@
 package com.antonpopoff.standstatsgenerator.views
 
-class StandStatRatingView {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.LinearLayout
+import com.antonpopoff.standstatsgenerator.R
+
+class StandStatRatingView(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : LinearLayout(context, attrs, defStyleAttr) {
+
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+
+    constructor(context: Context) : this(context, null)
+
+    init {
+        orientation = LinearLayout.HORIZONTAL
+        View.inflate(context, R.layout.layout_stand_stat_rating_view, this)
+    }
 }
