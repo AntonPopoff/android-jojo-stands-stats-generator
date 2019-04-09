af48441832d0d1f8767792ef0c3fdada99822e55
Anton Popoff <panton295@gmail.com>
2019-04-09 13:48:34 +0300

    Add base class for statistics popup

--- app/src/main/java/com/antonpopoff/standstatsgenerator/MainActivity.kt
+++ app/src/main/java/com/antonpopoff/standstatsgenerator/MainActivity.kt
@@ -2,11 +2,17 @@ package com.antonpopoff.standstatsgenerator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        standStatsDiagram.setOnClickListener {
+            StatisticsDialog().show(supportFragmentManager, null)
+        }
     }
 }
