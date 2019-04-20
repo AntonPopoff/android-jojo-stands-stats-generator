a0cffb025a706cd7dc75c9cf0baa450ba3e779b3
Anton Popoff <panton295@gmail.com>
2019-04-20 17:51:54 +0300

    Add edit button into toolbar in DiagramFragment

--- app/src/main/java/com/antonpopoff/standstatsgenerator/ui/DiagramFragment.kt
+++ app/src/main/java/com/antonpopoff/standstatsgenerator/ui/DiagramFragment.kt
@@ -1,9 +1,29 @@
 package com.antonpopoff.standstatsgenerator.ui
 
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import com.antonpopoff.standstatsgenerator.R
 import com.antonpopoff.standstatsgenerator.common.BaseViewFragment
+import kotlinx.android.synthetic.main.fragment_diagram.*
 
 class DiagramFragment : BaseViewFragment() {
 
     override val layoutId = R.layout.fragment_diagram
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupToolbarMenu()
+    }
+
+    private fun setupToolbarMenu() {
+        toolbar.apply {
+            inflateMenu(R.menu.fragment_diagram_toolbar_menu)
+            setOnMenuItemClickListener { onToolbarMenuClick(it) }
+        }
+    }
+
+    private fun onToolbarMenuClick(menuItem: MenuItem): Boolean {
+        return false
+    }
 }
