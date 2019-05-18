26454e53d10bd5981ffbe3d7c9412ac85379d914
Anton Popoff <panton295@gmail.com>
2019-05-18 19:34:19 +0300

    Optimize imports

--- colorwheel/src/main/java/com/antonpopoff/colorwheel/AlphaSeekBar.kt
+++ colorwheel/src/main/java/com/antonpopoff/colorwheel/AlphaSeekBar.kt
@@ -1,7 +1,6 @@
 package com.antonpopoff.colorwheel
 
 import android.content.Context
-import android.content.res.TypedArray
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Rect
