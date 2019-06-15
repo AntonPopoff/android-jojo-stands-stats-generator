66cacf3af39e2a309980afa51cef21d5972feb74
Anton Popov <panton295@gmail.com>
2019-06-15 20:20:24 +0300

    Fix dialog window

--- app/src/main/java/com/antonpopoff/standparametersgenerator/ui/TestDialog.kt
+++ app/src/main/java/com/antonpopoff/standparametersgenerator/ui/TestDialog.kt
@@ -2,16 +2,17 @@ package com.antonpopoff.standparametersgenerator.ui
 
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.KeyEvent
-import android.view.ViewGroup
-import android.view.WindowManager
+import android.view.*
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.view.animation.DecelerateInterpolator
 import android.view.animation.TranslateAnimation
 import com.antonpopoff.standparametersgenerator.R
 import com.antonpopoff.standparametersgenerator.utils.EmptyAnimationListener
+
 
 class TestDialog(context: Context) : Dialog(context) {
 
@@ -21,8 +22,12 @@ class TestDialog(context: Context) : Dialog(context) {
     private var root: ViewGroup? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        window?.requestFeature(Window.FEATURE_NO_TITLE)
+
         root = layoutInflater.inflate(R.layout.dialog_fragment_edit_diagram_color, null) as ViewGroup
-        setContentView(root)
+        root?.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 
         root?.setOnApplyWindowInsetsListener { v, insets ->
             if (insets.hasInsets()) {
@@ -32,37 +37,24 @@ class TestDialog(context: Context) : Dialog(context) {
             insets.consumeSystemWindowInsets()
         }
 
-        root?.setOnClickListener {
-            val translateAnimation = TranslateAnimation(0, 0f, 0, 0f, TranslateAnimation.RELATIVE_TO_SELF, 0f, TranslateAnimation.RELATIVE_TO_SELF, 1f)
-            translateAnimation.interpolator = decelerateInterpolator
-            translateAnimation.duration = 350
+        root?.setOnClickListener { startDismissAnimation() }
 
-            val alphaAnimation = AlphaAnimation(1f, 0f)
-            alphaAnimation.interpolator = decelerateInterpolator
-            alphaAnimation.duration = 350
-
-            alphaAnimation.setAnimationListener(dismissAnimationListener)
-
-            val contentView = root?.getChildAt(1)
-            val backgroundView = root?.getChildAt(0)
-
-            contentView?.startAnimation(translateAnimation)
-            backgroundView?.startAnimation(alphaAnimation)
-        }
+        setContentView(root)
 
         window?.apply {
             setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT)
-            setWindowAnimations(0)
-            setBackgroundDrawable(null)
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
             clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
             addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         }
     }
 
     override fun show() {
         super.show()
+        startShowAnimation()
+    }
 
+    private fun startShowAnimation() {
         val translateAnimation = TranslateAnimation(0, 0f, 0, 0f, TranslateAnimation.RELATIVE_TO_SELF, 1f, TranslateAnimation.RELATIVE_TO_SELF, 0f)
         translateAnimation.interpolator = decelerateInterpolator
         translateAnimation.duration = 350
@@ -80,26 +72,28 @@ class TestDialog(context: Context) : Dialog(context) {
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
         if (keyCode == KeyEvent.KEYCODE_BACK && event.action == KeyEvent.ACTION_DOWN) {
-            val translateAnimation = TranslateAnimation(0, 0f, 0, 0f, TranslateAnimation.RELATIVE_TO_SELF, 0f, TranslateAnimation.RELATIVE_TO_SELF, 1f)
-            translateAnimation.interpolator = decelerateInterpolator
-            translateAnimation.duration = 350
-
-            val alphaAnimation = AlphaAnimation(1f, 0f)
-            alphaAnimation.interpolator = decelerateInterpolator
-            alphaAnimation.duration = 350
-
-            alphaAnimation.setAnimationListener(dismissAnimationListener)
-
-            val contentView = root?.getChildAt(1)
-            val backgroundView = root?.getChildAt(0)
-
-            contentView?.startAnimation(translateAnimation)
-            backgroundView?.startAnimation(alphaAnimation)
-
+            startDismissAnimation()
             return true
         }
 
         return super.onKeyDown(keyCode, event)
+    }
+
+    private fun startDismissAnimation() {
+        val translateAnimation = TranslateAnimation(0, 0f, 0, 0f, TranslateAnimation.RELATIVE_TO_SELF, 0f, TranslateAnimation.RELATIVE_TO_SELF, 1f)
+        translateAnimation.interpolator = decelerateInterpolator
+        translateAnimation.duration = 350
+        translateAnimation.setAnimationListener(dismissAnimationListener)
+
+        val alphaAnimation = AlphaAnimation(1f, 0f)
+        alphaAnimation.interpolator = decelerateInterpolator
+        alphaAnimation.duration = 350
+
+        val contentView = root?.getChildAt(1)
+        val backgroundView = root?.getChildAt(0)
+
+        contentView?.startAnimation(translateAnimation)
+        backgroundView?.startAnimation(alphaAnimation)
     }
 
     private class DismissAnimationListener(private val dialog: Dialog) : EmptyAnimationListener {
